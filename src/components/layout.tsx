1cf28cf7fb1411a57eeeb45e1726439a9da4ef68
Hitesh  Zade <59929970+Hitesh-Zade@users.noreply.github.com>
2025-11-03 10:18:12 +0530

    Change footer copyright text to 2025
    Updated footer text to reflect copyright for 2025.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({children}: PropsWithChildren) => {
         </main>
         <footer className='border-t backdrop-blur py-2 supports-[backdrop-filter]:bg-background/50'>
             <div className='container mx-auto px-4 py-2 text-center text-muted-foreground'>
-      <p>Crafted with ❤️ and a lot of ☕</p>
+      <p>© 2025 Hitesh Zade. All rights reserved.</p>
             </div>
         </footer>
         </div>
